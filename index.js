2421c4ec9d246543dd2ad5892250e9c25e66e196
tommyxlanes <tommyxlanes@gmail.com>
2024-05-28 20:06:28 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const section = document.querySelector('section');
+
+function callMobile() {
+  section.style.backgroundColor = 'yellowgreen';
+  console.log('Mobile running');
+}
+
+function callDesktop() {
+  section.style.backgroundColor = 'coral';
+  console.log('Desktop running');
+}
+
+function getInfo() {
+  if (window.innerWidth <= 430) {
+    callMobile();
+  } else {
+    callDesktop();
+  }
+}
+
+let resizeTimeout;
+window.addEventListener('resize', () => {
+  clearTimeout(resizeTimeout);
+  resizeTimeout = setTimeout(getInfo, 100, 'Resized');
+});
+
+// window.onload = getInfo;
+// window.onresize = getInfo;
